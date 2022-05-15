ecd3b55ebee4e7357c69f9f741fcbd37649ddc3e
Zhenhua Yang <huaouo@live.com>
2022-05-15 15:26:49 +0800

    Update the proto compiling script

--- protoc_recompile.sh
+++ protoc_recompile.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-cd "${0%/*}" || exit
+pushd "${0%/*}" >>/dev/null || exit
 
 for d in *; do
   if [ -d "$d/proto" ]; then
-    pushd "$d" > /dev/null || exit
+    pushd "$d" >/dev/null || exit
     echo "recompiling $d/proto ..."
     protoc --go_out=. --go-grpc_out=. proto/*
-    popd > /dev/null || exit
+    popd >/dev/null || exit
   fi
 done
+
+popd >/dev/null || exit
